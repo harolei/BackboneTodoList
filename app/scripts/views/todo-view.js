09cb51ce48750e689b65b949088dcd0e779c9836
harolei <harolei@gmail.com>
2014-05-03 08:54:33 +0800

    Finished mark item as completed.

--- app/scripts/views/todo-view.js
+++ app/scripts/views/todo-view.js
@@ -2,17 +2,31 @@ todoApp = todoApp || {};
 
 define(['underscore', 'backbone'], function(_, Backbone) {
     return Backbone.View.extend({
-        tagName:  'li',
+        tagName: 'li',
         template: _.template($('#item-template').html()),
         initialize: function() {
             this.$el.html(this.template(this.model.toJSON()));
+            this.$input = this.$('.edit');
             this.listenTo(this.model, 'destroy', this.remove);
+            this.listenTo(this.model, 'change:completed', this.setToggleClass)
         },
         events: {
-            'click .destroy': 'deleteItem'
+            'click .destroy': 'deleteItem',
+            'click .toggle': 'completeItem',
+            'dblclick label': 'editItem'
         },
-        deleteItem: function(){
+        deleteItem: function() {
             this.model.destroy();
+        },
+        completeItem: function() {
+            this.model.set('completed',!this.model.get('completed'));
+        },
+        setToggleClass: function() {
+            this.$el.toggleClass('completed', this.model.get('completed'));
+        },
+        editItem: function() {
+            this.$el.addClass('editing');
+            this.$input.focus();
         }
     });
 });
